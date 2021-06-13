a2794c08e287f69165b5c51082d194f04ff42957
simplyjuanc <juancamilovasquezj@gmail.com>
2021-06-13 22:37:21 +0100

    Updates on the prev commit

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,5 @@
-from mimetypes import init
 import os 
-from flask import Flask
+from flask import Flask, session
 from flask_session import Session
 
 def create_app():
@@ -9,13 +8,9 @@ def create_app():
     app.config['CLIENT_ID'] = os.getenv('CLIENT_ID')
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-    with app.app_context():
-        # init_db()
 
-        from .auth import auth as auth_blueprint
-        app.register_blueprint(auth_blueprint)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-        from .main import main as main_blueprint
-        app.register_blueprint(main_blueprint)
 
     return app
--- project/main.py
+++ project/main.py
@@ -1,36 +1,90 @@
+import os
+from re import M
+from flask import Flask, redirect, request, session
+from flask_session import Session
 import requests, json
+from urllib.parse import urlparse, parse_qs
 
-from flask import Blueprint
-from flask.globals import session
-
-from . import auth
-
-
-main = Blueprint('main', __name__)
+# if __name__ == '__main__':
+main = Flask(__name__)
+# main.config['CLIENT_ID'] = os.getenv('CLIENT_ID')
+# main.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+main.secret_key = os.urandom(24).hex()
+main.config['SESSION_TYPE'] = 'filesystem'
+Session(main)
 
 BASE_URL = 'https://api.spotify.com/v1/'
-header = {
-    'Authorization': 'Bearer {}'.format(session.get('token_data', None)),
-    'Accept':'application/json',
-    'Content-Type': 'application/json'
+REDIRECT_URI = 'http://localhost:5000/redirect'
+SCOPES = 'user-read-recently-played'
+
+# header = {
+#     # 'Authorization': 'Bearer {}'.format(session['token_data']['access_token']),
+#     'Accept':'application/json',
+#     'Content-Type': 'application/json'
+#     }
+
+
+@main.route('/authorise')
+def getAccessToken():
+    AUTH_URL = 'https://accounts.spotify.com/authorize'
+    payload = {
+        'client_id':os.getenv('CLIENT_ID'),
+        'response_type':'code',
+        'redirect_uri':REDIRECT_URI,
+        'scope':SCOPES,
     }
+
+    auth_response = requests.get(AUTH_URL, data=payload)
+
+    # response_url = urlparse.urlparse(auth_response.url)   
+    # print(response_url)
+    
+
+
+    # session['code'] = auth_response_data['code']
+    return redirect('/redirect')
+
+
+
+@main.route('/redirect')
+def getAuthCode():
+    TOKEN_URL = 'https://accounts.spotify.com/api/token'
+    data = {
+        'grant_type':'authorization_code',
+        'code':request.args['code'],
+        'redirect_uri':REDIRECT_URI,
+        'client_id':os.getenv('CLIENT_ID'),
+        'client_secret':os.getenv('SECRET_KEY')
+    } 
+    token_response = requests.post(TOKEN_URL, data=data)
+    token_json = json.loads(token_response.text)
+    # print(token_json)
+    session['access_token'] = token_json['access_token']
+    session.modified = True
+    session['refresh_token'] = token_json['refresh_token']
+    session.modified = True
+    
+    return str(session.items())
+
 
 @main.route('/trackhistory')
 def obtainTrackHistory():
     call_url = BASE_URL + 'me/player/recently-played'
-    
-    # header = {
-    #     'Authorization': 'Bearer ' + session['token_data']['access_token'],
-    #     'Accept':'application/json',
-    #     'Content-Type': 'application/json'
-    #     }
+    token = session.get('access_token')
+    print(session.keys())
+
+    header = {
+        'Authorization': 'Bearer {}'.format(token),
+        'Accept':'application/json',
+        'Content-Type': 'application/json'
+        }
 
     data = {
         'limit':50
     }
 
     response = requests.get(call_url, headers=header, params=data)
-    # response_json = response.text
+    response_json = response.text
     response_json = json.loads(response.text)
 
     tracks = {}
@@ -39,8 +93,8 @@ def obtainTrackHistory():
         tracks[track_name] = {'id':track['track']['id']}
 
     session['TRACK_HISTORY'] = tracks
-
-    return 'I have saved session variable for tracks history.\n\n' + str(tracks)
+    session.modified = True
+    return 'I have saved session variable for tracks history.' #+ str(tracks)
 
 
 @main.route('/trackanalysis')
@@ -70,4 +124,13 @@ def averageTrackTraits():
 @main.route('/visualisetraits')
 def visualiseTrackTraits():
     pass
+
+@main.route('/sess1')
+def session1():
+    return session.sid
+
+@main.route('/sess2')
+def session2():
+    return session.sid
+
 
