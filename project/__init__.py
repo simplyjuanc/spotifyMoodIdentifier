df7baaceec2e0a084f2dc5486c34737a23d2ffa8
simplyjuanc <juancamilovasquezj@gmail.com>
2021-06-14 23:15:15 +0100

    save _init_ file

--- project/__init__.py
+++ project/__init__.py
@@ -1,14 +1,16 @@
 import os 
 from flask import Flask
-from flask_session import Session
+
 
 def create_app():
     app = Flask(__name__)
 
     app.config['CLIENT_ID'] = os.getenv('CLIENT_ID')
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    SECRET_KEY = app.config['SECRET_KEY']
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
 
     return app
